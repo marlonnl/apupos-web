9f9c0569733036f6298b1abb02a74666223383ab
Marlonn Locatelli <marlonn@gmail.com>
2025-06-18 18:41:07 -0300

    ✨ GlobalStyle and colors

---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+    list-style-type: none;
+    // TODO: add Isoevka
+  }
+
+  .container {
+    max-width: 800px;
+    width: 100%;
+    margin: 0 auto;
+  }
+`
