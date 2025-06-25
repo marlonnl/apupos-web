5763dcf45b9643667e3335e3aa058c4016dbc28e
Marlonn Locatelli <marlonn@gmail.com>
2025-06-25 14:59:41 -0300

    ✨🚧  rendering posts from API

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -1,53 +1,54 @@
-import { ArrowRepeat, Chat, Heart } from 'react-bootstrap-icons'
-import {
-  ActionIconContainer,
-  PostActions,
-  PostContainer,
-  PostContent,
-  PostListContainer
-} from './styles'
+import { PostListContainer } from './styles'
+import { useEffect, useState } from 'react'
+import Apupo from '../Apupo'
 
 const PostList = () => {
-  const posts = [
-    {
-      content: 'Sejam bem vindos ao microblogging APUPOS.',
-      user: 'joao',
-      name: 'Joao Joao'
-    },
-    { content: 'segunda postagem', user: 'mariam', name: 'Maria Maria' }
-  ]
+  function loadApupos(callback: any) {
+    const xhr = new XMLHttpRequest()
+    const method = 'GET'
+    const url = 'http://localhost:8000/api/apupo'
+    const responseType = 'json'
+
+    xhr.responseType = responseType
+    xhr.open(method, url)
+    xhr.onload = function () {
+      console.log(xhr.response)
+      callback(xhr.response, xhr.status)
+    }
+    xhr.onerror = function (error) {
+      callback({ content: 'the request was an error' }, 400)
+    }
+    xhr.send()
+  }
+
+  const [posts, setPosts] = useState<PostsAPI[]>()
+
+  // setPosts({ content: 'loading' })
+
+  useEffect(() => {
+    const myCallback = (response: PostsAPI[], status: number) => {
+      console.log(response)
+      if (status === 200) {
+        setPosts(response)
+      } else {
+        alert('Ocorreu um erro.')
+      }
+    }
+    loadApupos(myCallback)
+  }, [])
 
   return (
     <PostListContainer>
-      {posts.map((post, index) => {
+      {posts?.map((post) => {
         return (
-          <>
-            <PostContainer key={index}>
-              <div>
-                <img src="https://cdn.bsky.app/img/avatar/plain/did:plc:fjye6cgixsgbtfa3pfbaeuko/bafkreibjobzsdumpa6b7v747gjvqxkpkjqd3nyailuyof7qgagvr42jby4@jpeg" />
-              </div>
-              <PostContent>
-                <div>
-                  <p className="name">{post.name}</p>
-                  <p className="user">@{post.user}</p>
-                  <p className="time">5m</p>
-                </div>
-                <p className="content">{post.content}</p>
-                <PostActions>
-                  <ActionIconContainer>
-                    <Chat />3
-                  </ActionIconContainer>
-                  <ActionIconContainer>
-                    <Heart />
-                    12
-                  </ActionIconContainer>
-                  <ActionIconContainer>
-                    <ArrowRepeat />6
-                  </ActionIconContainer>
-                </PostActions>
-              </PostContent>
-            </PostContainer>
-          </>
+          <Apupo
+            id={post.id}
+            content={post.content}
+            likes={post.likes}
+            is_retweet={post.is_retweet}
+            parent={post.parent}
+            key={post.id}
+          />
         )
       })}
     </PostListContainer>
