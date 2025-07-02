bc2f46e047a88ca4f8da99cf4b31933a7e921a8a
Marlonn Locatelli <marlonn@gmail.com>
2025-07-02 18:11:18 -0300

    🧹 removing comments

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -1,41 +1,10 @@
 import { PostListContainer } from './styles'
 import { useEffect, useState } from 'react'
 import Apupo from '../Apupo'
+import { useGetApuposQuery } from '../../services/api'
 
 const PostList = () => {
-  function loadApupos(callback: any) {
-    const xhr = new XMLHttpRequest()
-    const method = 'GET'
-    const url = 'http://localhost:8000/api/apupo'
-    const responseType = 'json'
-
-    xhr.responseType = responseType
-    xhr.open(method, url)
-    xhr.onload = function () {
-      console.log(xhr.response)
-      callback(xhr.response, xhr.status)
-    }
-    xhr.onerror = function (error) {
-      callback({ content: 'the request was an error' }, 400)
-    }
-    xhr.send()
-  }
-
-  const [posts, setPosts] = useState<PostsAPI[]>()
-
-  // setPosts({ content: 'loading' })
-
-  useEffect(() => {
-    const myCallback = (response: PostsAPI[], status: number) => {
-      console.log(response)
-      if (status === 200) {
-        setPosts(response)
-      } else {
-        alert('Ocorreu um erro.')
-      }
-    }
-    loadApupos(myCallback)
-  }, [])
+  const { data: posts } = useGetApuposQuery()
 
   return (
     <PostListContainer>
