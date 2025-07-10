4e7a4ea419a8d9b801c510aa24df61e500f421da
Marlonn Locatelli <marlonn@gmail.com>
2025-07-10 20:27:48 -0300

    ✨ route to detailed post page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
+import Post from './pages/Post'
 
 const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
+    <Route path="/post/:postId" element={<Post />} />
   </Routes>
 )
 
