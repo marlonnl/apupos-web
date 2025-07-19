8b6505ec26cda87ef4fe1394a5a737242e5e0216
Marlonn Locatelli <marlonn@gmail.com>
2025-07-18 23:10:42 -0300

    ✨ animation on global style

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -26,6 +26,32 @@ export const GlobalStyle = createGlobalStyle`
     width: 100%;
     margin: 0 auto;
   }
+
+  #animation {
+    margin-bottom: 8px;
+  }
+
+  #animation {
+    background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+    padding: 6px;
+    border-radius: 50%;
+    background-size: 400% 400%;
+    width: 32px;
+    margin: 0 auto;
+    animation: gradient 15s ease infinite;
+  }
+
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
 `
 
 export const Main = styled.div`
