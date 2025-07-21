816b6749c8546b4eaaba165d0790c73fc0101301
Marlonn Locatelli <marlonn@gmail.com>
2025-07-21 17:49:05 -0300

    ✨ added route to login page

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Post from './pages/Post'
 import Register from './pages/Register'
+import Login from './pages/Login'
 
 const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/post/:postId" element={<Post />} />
     <Route path="/register" element={<Register />} />
+    <Route path="/login" element={<Login />} />
   </Routes>
 )
 
