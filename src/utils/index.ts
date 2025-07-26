bb3767c5466844370f6ccb4b5b16643214534c5a
Marlonn Locatelli <marlonn@gmail.com>
2025-07-26 15:02:30 -0300

    ✨ BASE_API_URL as global const

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,4 +2,11 @@ export const formatDate = (date: string) => {
   return date
 }
 
+export const BASE_API_URL = 'http://localhost:8000/api'
 export const MAX_LENGTH = 240
+
+// export const handleLogout = async () => {
+//   try {
+
+//   }
+// }
