013a728496ff0dbedc8b19edae1655f9ce3385d4
Marlonn Locatelli <marlonn@gmail.com>
2025-08-02 17:17:03 -0300

    ✨ added /user/<username> route

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,11 +4,13 @@ import Home from './pages/Home'
 import Post from './pages/Post'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import User from './pages/User'
 
 const Rotas = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/post/:postId" element={<Post />} />
+    <Route path="/user/:username" element={<User />} />
     <Route path="/register" element={<Register />} />
     <Route path="/login" element={<Login />} />
   </Routes>
