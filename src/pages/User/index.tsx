66f1b5d9be8c2212bb2730122eed30c1601281cb
Marlonn Locatelli <marlonn@gmail.com>
2025-08-04 18:01:22 -0300

    ✨ fetching data

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom'
+import { Params, useParams } from 'react-router-dom'
 import Sidebar from '../../components/Sidebar'
 import { Main } from '../../styles'
 import {
@@ -15,57 +15,81 @@ import {
   PersonFillAdd,
   PersonLinesFill
 } from 'react-bootstrap-icons'
+import { useState } from 'react'
+import EditProfile from '../../components/EditProfile'
+import { useGetProfileQuery } from '../../services/api_profile'
 
 const User = () => {
-  const { username } = useParams()
+  const { username } = useParams() as { username: string }
+
+  const [modalVisible, setModalVisible] = useState<boolean>(false)
+  const { data } = useGetProfileQuery(username)
+
+  function onClickEditProfile() {
+    setModalVisible(true)
+  }
+
+  const closeModal = () => {
+    setModalVisible(false)
+  }
 
   return (
-    <div className="container">
-      <Main>
-        <Sidebar />
-        <UserContainer>
-          <UserHeader>
-            <div style={{ marginBottom: '16px' }}>
-              <ArrowLeft size={22} />
-            </div>
-            <UserHeaderNavContainer>
-              <img src="https://cdn.bsky.app/img/avatar/plain/did:plc:fjye6cgixsgbtfa3pfbaeuko/bafkreibjobzsdumpa6b7v747gjvqxkpkjqd3nyailuyof7qgagvr42jby4@jpeg" />
-              <UserHeaderNavbar>
-                <button>
-                  <PersonLinesFill /> editar perfil
-                </button>
-                <button>
-                  <PersonFillAdd /> seguir
-                </button>
-                <button>...</button>
-              </UserHeaderNavbar>
-            </UserHeaderNavContainer>
-          </UserHeader>
-          <UserInfo>
-            <h2>NOME DO USUARIO</h2>
-            <div>
-              <h3>@{username}</h3>
-              <span className="doesfollow">segue você</span>
-            </div>
-            <UserInfoItem>
-              <a href="#">
-                <span className="number">0</span>
-                <span>&nbsp;seguidores</span>
-              </a>
-              <a href="#">
-                <span className="number">0</span>
-                <span>&nbsp;seguindo</span>
-              </a>
-              <a href="#">
-                <span className="number">0</span>
-                <span>&nbsp;apupos</span>
-              </a>
-            </UserInfoItem>
-            <UserBio>asaaaaaaaaaa</UserBio>
-          </UserInfo>
-        </UserContainer>
-      </Main>
-    </div>
+    <>
+      <div
+        className="container"
+        style={modalVisible ? { opacity: '0.3' } : { opacity: '1' }}
+      >
+        <Main>
+          <Sidebar />
+          <UserContainer>
+            {data && (
+              <>
+                <UserHeader>
+                  <div style={{ marginBottom: '16px' }}>
+                    <ArrowLeft size={22} />
+                  </div>
+                  <UserHeaderNavContainer>
+                    <img src="https://cdn.bsky.app/img/avatar/plain/did:plc:fjye6cgixsgbtfa3pfbaeuko/bafkreibjobzsdumpa6b7v747gjvqxkpkjqd3nyailuyof7qgagvr42jby4@jpeg" />
+                    <UserHeaderNavbar>
+                      <button onClick={onClickEditProfile}>
+                        <PersonLinesFill /> editar perfil
+                      </button>
+                      <button>
+                        <PersonFillAdd /> seguir
+                      </button>
+                      <button>...</button>
+                    </UserHeaderNavbar>
+                  </UserHeaderNavContainer>
+                </UserHeader>
+                <UserInfo>
+                  <h2>{data[0].name}</h2>
+                  <div>
+                    <h3>@{username}</h3>
+                    <span className="doesfollow">segue você</span>
+                  </div>
+                  <UserInfoItem>
+                    <a href="#">
+                      <span className="number">0</span>
+                      <span>&nbsp;seguidores</span>
+                    </a>
+                    <a href="#">
+                      <span className="number">0</span>
+                      <span>&nbsp;seguindo</span>
+                    </a>
+                    <a href="#">
+                      <span className="number">0</span>
+                      <span>&nbsp;apupos</span>
+                    </a>
+                  </UserInfoItem>
+                  <UserBio>{data[0].bio}</UserBio>
+                </UserInfo>
+              </>
+            )}
+          </UserContainer>
+        </Main>
+      </div>
+      <div>{modalVisible && <EditProfile />}</div>
+    </>
   )
 }
 
