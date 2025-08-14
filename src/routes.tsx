0426bb1ead370028526c2dac00c9c1d79b0115bd
Marlonn Locatelli <marlonn@gmail.com>
2025-08-14 14:26:14 -0300

    ✨ added authentitaction verification and storage inside routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,44 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Post from './pages/Post'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import User from './pages/User'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { useAuthenticatedQuery } from './services/api_auth'
+import { authentication } from './store/reducers/auth'
 
-const Rotas = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/post/:postId" element={<Post />} />
-    <Route path="/user/:username" element={<User />} />
-    <Route path="/register" element={<Register />} />
-    <Route path="/login" element={<Login />} />
-  </Routes>
-)
+const Rotas = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const { isSuccess, data, status, isError, isFetching } =
+    useAuthenticatedQuery()
+
+  useEffect(() => {
+    if (status === 'rejected') {
+      navigate('/login')
+    } else if (data) {
+      const authData = {
+        authenticated: true,
+        user: data.user
+      }
+
+      dispatch(authentication(authData))
+    }
+  }, [data])
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/post/:postId" element={<Post />} />
+      <Route path="/user/:username" element={<User />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  )
+}
 
 export default Rotas
